ef0dfa980e518dd82d8442578c725cf32ad53304
SosoLaMojo <sol.sch1990@gmail.com>
2019-10-02 15:00:33 +0200

    Creation du GIT et du fichier CPP

---
+++ 7.0 RogueLike/7.0 RogueLike/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+/* EXERCICE 7.0 ROGUE LIKE
+Créer un jeu dans le style roguelike.
+Vous devez afficher une map dans la console, vous pouvez choisir le taille, et ce que vous mettez dedans,
+mais il faut que celle-ci soit rafraîchie au moins à chaque déplacement du personnage. N’oubliez pas de nettoyer la console avant d’afficher quelque chose.
+Pour le personnage principale il faut qu’il puisse effectuer des actions (command-based style).
+Il doit être capable de se déplacer et de prendre des objets. Il doit être présenter par un symbole précis.
+Le joueur doit être capable de récupérer des potions de vie s’il est sur la même case(via une action) et
+s’il marche sur un piège il faut que celui-ci perde de la vie. Il faut donc aussi afficher sa vie.
+En plus du projet il faut rendre un document en format pdf avec un schéma qui explique comment les jeux s'exécute et
+comment les classes sont liées entre elles.
+Une fois les éléments de bases terminé (map, personnage, déplacement, UI) vous êtes libre de faire ce que vous voulez.
+N’oubliez pas de faire quelque chose de fun et amusez-vous en le faisant.
+*/
+
+int main() {
+
+
+
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
